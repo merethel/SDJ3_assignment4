01777769329c69d07dd9f917aff6def58b3b6f6e
Merethe Lundgreen <299922@via.dk>
2022-11-14 13:02:21 +0100

    Creation of assignment

---
+++ src/main/java/com/example/sdj3_assignment4/Sdj3Assignment4Application.java
@@ -0,0 +1,13 @@
+package com.example.sdj3_assignment4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdj3Assignment4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdj3Assignment4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sdj3_assignment4/Sdj3Assignment4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdj3_assignment4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdj3Assignment4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
